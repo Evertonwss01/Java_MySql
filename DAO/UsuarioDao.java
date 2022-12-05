00dcd5847499f0b4c66c789b8190328d4d613e8f
Evertonwss01 <evertonwss2001@gmail.com>
2022-12-05 15:24:33 -0300

    Modificacao
    Modificação e Adicionando retornos do tipo Int e uma Instacia de um proprio Usuario.
    E criando mais um Metodo de Pegar um Usuario Espscifico, como uma Pesquisa atravez do Id.

--- DAO/UsuarioDao.java
+++ DAO/UsuarioDao.java
@@ -1,21 +1,24 @@
-package br.com.DAO;					// Pacote Atual //
-import br.com.ConexaoBanco.*;		// Pacote Importado para trazer a clsse Conexao com o Banco de Dados //
-import br.com.modelos.*;			// Pacote Importado para trazer a classe Modelo "Usuario" //
+package com.Crudjsp.Dao;					// Pacote Atual //
+import com.Crudjsp.modelos.Usuario;
+import com.Crudjsp.Dao.Conexao;				// Pacote de Conexao com o Banco //
+import com.Crudjsp.modelos.*;				// Pacote dos Modelos //
 
-
+//import java.util.Date;
 import java.sql.Connection;			// Pacote de Conexao com o Banco SQL //
 import java.sql.PreparedStatement;	// Pacote de Preparacao de Query para ser executada no Banco de Dados //
-import java.sql.ResultSet;			// Pacote
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.ResultSet;			// Pacote de Retorno do Banco de Dados o ResultSet //
+import java.sql.SQLException;		// Exceção de MySql //
+import java.util.ArrayList;			// ArrayLista para Listar todos os Registros do Banco //
+import java.util.List;				// O tipo List //
 import java.sql.Date;				// Pacote para trazer a Data Atual //
 
 public class UsuarioDao {
 
 	// Metodo Criate //
-	public void Criar(Usuario uso) {
+	public static int Criar(Usuario uso) {
 		
+		// Essa Variavel guardará o resultado 0 para Erro ou 1 para certo, será util para utilizamos na paginas WEB //
+		int status =0;
 		// Variavel String com o comando MySql //
 		String sql = "INSERT INTO Usuario (nome, username, password, dataRegistro)values( ?, ?, ?, ?)";
 		
@@ -33,7 +36,7 @@ public class UsuarioDao {
 			pstm.setString(3, uso.getSenha());
 			pstm.setDate(4, new Date(uso.getDataRegistro().getTime()));
 		// Excuta a Query // 
-			pstm.execute();
+			status = pstm.executeUpdate();
 			System.out.println("Dados Salvos com Sucesso !");
 		}catch(Exception e) {
 			e.printStackTrace();;
@@ -54,6 +57,36 @@ public class UsuarioDao {
 				w.getMessage();
 			}
 		}
+		return status;
+	}
+	
+	// ###################  Pegar um Usuario Especifico como uma busca  ##################### //
+	
+	public static Usuario PegarUsuarioEspacifico(int id) {
+		Usuario usu = null;
+		
+		String sql = "SELECT * FROM Usuario WHERE id =?";
+		Connection conexao = null;
+		PreparedStatement pstm = null;
+		try {
+			conexao = Conexao.createConnectionMySQL();
+			pstm = conexao.prepareStatement(sql);
+			pstm.setInt(1, id);
+			
+			pstm.execute();
+			ResultSet retorno = pstm.executeQuery();
+			while(retorno.next()) {
+				// Cria uma Instacia de Usuario //
+				usu = new Usuario();
+				// Passar os Valores //
+				usu.setId(retorno.getInt("id"));
+				usu.setNome(retorno.getString("nome"));
+				usu.setLogin(retorno.getString("username"));
+				usu.setSenha(retorno.getString("password"));
+			}
+		}catch(Exception e) {e.printStackTrace();}
+	return usu;		
+			
 	}
 	
 	
@@ -106,8 +139,8 @@ public class UsuarioDao {
 	
 
 	// Metodo Update //
-	public void Atualizar(Usuario uso) {
-		
+	public static int Atualizar(Usuario uso) {
+		int status =0;
 		String slq = "UPDATE Usuario SET nome = ?, username = ?, password = ?, dataRegistro = ? WHERE id = ?";
 		
 		Connection conexao = null;
@@ -122,9 +155,9 @@ public class UsuarioDao {
 			pstm.setDate(4, new Date(uso.getDataRegistro().getTime()));
 			
 			// Informar qual será o Id, que será modificado //
-			pstm.setInt(5, uso.getId());
+			pstm.setInt(4, uso.getId());
 			
-			pstm.execute();
+			status = pstm.executeUpdate();
 			System.out.println("Atualização Concluida !");
 		}catch(SQLException w) {
 			w.printStackTrace();
@@ -141,11 +174,13 @@ public class UsuarioDao {
 				e.printStackTrace();
 			}
 		}
+		return status;
 	}
 	
 	
 	// Metodo Delete //
-	public void Apagar(int id) {
+	public static int Apagar(int id) {
+		int status =0;
 		String sql = "DELETE FROM Usuario WHERE id = ?";
 		
 		Connection conexao = null;
@@ -157,7 +192,6 @@ public class UsuarioDao {
 			pstm.setInt(1, id);
 
 			pstm.executeQuery();
-			System.out.println("Dados apagados com Sucesso !");
 		}catch(Exception w){
 			w.printStackTrace();
 		}
@@ -173,5 +207,6 @@ public class UsuarioDao {
 				e.printStackTrace();
 			}
 		}
+		return status;
 	}
 }
